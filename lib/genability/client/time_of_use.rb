bed0e08a698e31cb565d31e572ec91447a2e97d9
Matthew Solt <mattsolt@gmail.com>
2011-07-01 14:37:30 -0400

    refactored and documented time of use methods

--- lib/genability/client/time_of_use.rb
+++ lib/genability/client/time_of_use.rb
@@ -1,26 +1,64 @@
 module Genability
   class Client
-
+    # Many tariffs have pricing that depends on the time of day the energy
+    # is being used. We call these times the Time of Use for a tariff.
+    # The most common examples are On Peak and Off Peak. Some examples
+    # here may help:
+    #
+    #   1. Within a single Time of Use, e.g. On Peak, the price for a
+    #      tariff will always be the same.
+    #   2. Within a single Time of Use, e.g. On Peak, you may have multiple
+    #      Periods. A Period is a range of days and times that this TOU applies to.
     module TimeOfUse
 
+      # Returns a particular time of use group using its time of use group ID and the load serving entity ID.
+      #
+      # @format :json
+      # @authenticated true
+      # @rate_limited true
+      # @param load_serving_entity_id [Integer] Unique Genability ID (primary key) for a Load Serving Entity.
+      # @param time_of_use_group_id [Integer] Genability ID (primary key) for this Time of Use Group. This is unique within the LSE, not across LSE's so you will always need to specify the LSE ID when requested a TOU Group.
+      # @return [Hashie::Mash] Return the time of uses for a load serving entity.
+      # @see https://developer.genability.com/documentation/api-reference/public/time-of-use
+      # @example Return the time of use group for Georgia Power Co
+      #   Genability.time_of_uses(2756, 1)
       def time_of_uses(load_serving_entity_id, time_of_use_group_id)
-        params["pageStart"] = params[:page] if params[:page]
-        params["pageCount"] = params[:per_page] if params[:per_page]
-        get("time_of_uses/#{load_serving_entity_id}/#{time_of_use_group_id}", params).results
+        get("timeofuses/#{load_serving_entity_id}/#{time_of_use_group_id}").results.first
       end
 
       alias :tou :time_of_uses
 
-      # fromDateTime Date ISO 8601 format for the starting date and time of the requested Intervals. Defaults to current day and time if not specified. (Optional)
-      # toDateTime Date ISO 8601 format for the ending date and time of the requested Intervals. Defaults to one week after the fromDateTime. (Optional)
-      def time_of_use_intervals(load_serving_entity_id, time_of_use_group_id, params = {})
-        params["fromDateTime"] = params[:from] if params[:from]
-        params["toDateTime"] = params[:to] if params[:to]
-        get("time_of_uses/#{load_serving_entity_id}/#{time_of_use_group_id}/intervals", params).results
+      # Returns all the Intervals for a Time of Use Group for an optionally specified
+      # from and to date and time range. Defaults to current time if fromDateTime is
+      # not specified and to a one week look ahead window if toDateTime is not specified.
+      #
+      # @format :json
+      # @authenticated true
+      # @rate_limited true
+      # @param load_serving_entity_id [Integer] Unique Genability ID (primary key) for a Load Serving Entity.
+      # @param time_of_use_group_id [Integer] Genability ID (primary key) for this Time of Use Group. This is unique within the LSE, not across LSE's so you will always need to specify the LSE ID when requested a TOU Group.
+      # @param options [Hash] A customizable set of options.
+      # @option options [DateTime] :from ISO 8601 format for the starting date and time of the requested Intervals. Defaults to current day and time if not specified. (Optional)
+      # @option options [DateTime] :to ISO 8601 format for the ending date and time of the requested Intervals. Defaults to one week after the fromDateTime. (Optional)
+      # @return [Array] Returns all the Intervals for a Time of Use Group.
+      # @see https://developer.genability.com/documentation/api-reference/public/time-of-use
+      # @example Return the intervals for the time of use group for Georgia Power Co
+      #   Genability.time_of_use_intervals(2756, 1)
+      def time_of_use_intervals(load_serving_entity_id, time_of_use_group_id, options = {})
+        get("timeofuses/#{load_serving_entity_id}/#{time_of_use_group_id}/intervals", interval_params(options)).results
       end
 
       alias :tou_intervals :time_of_use_intervals
       alias :intervals :time_of_use_intervals
+
+      private
+
+      def interval_params(options)
+        {
+          'fromDateTime' => format_to_iso8601(options[:from]),
+          'toDateTime' => format_to_iso8601(options[:to])
+        }.delete_if{ |k,v| v.nil? }
+      end
 
     end
   end
---
+++ spec/client/time_of_use_spec.rb
@@ -0,0 +1,42 @@
+require File.expand_path('../../spec_helper', __FILE__)
+
+describe Genability::Client do
+
+  Genability::Configuration::VALID_FORMATS.each do |format|
+
+    context ".new(:format => '#{format}')" do
+
+      before(:all) do
+        @options = {:format => format}.merge(configuration_defaults)
+        @client = Genability::Client.new(@options)
+      end
+
+      context ".time_of_uses" do
+
+        use_vcr_cassette "time_of_uses"
+
+        it "should return an the time of uses for a given LSE and touGroupId" do
+          time_of_uses = @client.time_of_uses(2756, 1)
+          time_of_uses.should be_an Hashie::Mash
+          time_of_uses.time_of_uses.count.should == 2
+        end
+
+      end
+
+      context ".time_of_use" do
+
+        use_vcr_cassette "time_of_use"
+
+        it "should return a territory" do
+          intervals = @client.time_of_use_intervals(2756, 1)
+          intervals.should be_an Array
+          intervals.first.tou_group_id.should == 1
+        end
+
+      end
+
+    end
+
+  end
+end
+
